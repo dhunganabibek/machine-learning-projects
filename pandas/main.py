ccd2a80389c4602501992a03863a715944391ac9
Bibek Dhungana <Dhunganabibek2016@gmail.com>
2025-05-06 21:21:36 -0500

    pandas setup

--- pandas/main.py
+++ pandas/main.py
@@ -4,9 +4,5 @@ import numpy as np
 
 df = pd.read_csv('currency.csv')
 
-is_true = [False] * 163
-is_true[0] = True
-is_true_series = pd.Series(is_true) 
-
-
-print(df[is_true_series])
+for label, row in df.iterrows():
+    print(label, row['currency'])
